5939d639ee4e611426642d9025c2e2acb6c426d1
ligialopes88 <ligialopes88@gmail.com>
2026-04-06 18:27:48 +0000

    cirando nova aula

---
+++ aula_01/aula-01-2-media/aula-01-2.py
@@ -0,0 +1,9 @@
+nota_prova = float(input("Digite sua nota da prova: "))
+nota_trabalho = float(input"Digite a sua nota do trabalho: "))
+
+ media = (nota_prova + nota_trabalho) / 2
+
+if media >= 5: 
+    print ("APROVADO")
+else:
+   print ("REPROVADO") 
