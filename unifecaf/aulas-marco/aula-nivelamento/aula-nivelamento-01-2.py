56e70d790e40517e98f846c285a58d8e79ad1d4b
ligialopes88 <ligialopes88@gmail.com>
2026-04-07 19:38:55 +0000

    atualizando

--- unifecaf/aulas-marco/aula-nivelamento/aula-nivelamento-01-2.py
+++ unifecaf/aulas-marco/aula-nivelamento/aula-nivelamento-01-2.py
@@ -5,8 +5,8 @@ n1 = int(input("Digite o primeiro número: "))
 n2 = int(input("Digite o segundo número: "))
 
 if n1 > n2:
-    print(n1)
+    print("O primeiro número é maior!")
 elif n2 > n1: 
-    print(n2)
+    print("O segundo número é maior!")
 else: 
     print("São iguais!")
